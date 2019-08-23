bce965a9b80bb4de85cfed9b464dba696f654621
TheNerdThing <GregoryEvevsky@gmail.com>
2019-08-23 18:51:11 -0400

    combined everything into a single GUI

--- PLO8 FlopZilla/src/GUI.java
+++ PLO8 FlopZilla/src/GUI.java
@@ -1,6 +1,7 @@
 
 
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
 
 
 /**
@@ -11,13 +12,20 @@ public class GUI extends JFrame{
 
 	PLOhandSelector ploS;
 	FlopSelector fs;
+	
+	JTabbedPane tabbedPane; 
 
 	public GUI() {
+		tabbedPane = new JTabbedPane(); 
+
 		this.setBounds(450,10,1000,1000);
 		ploS =  new PLOhandSelector(300,300,0,0);
 		fs = new FlopSelector(500,250,0,0);
-		this.add(fs);
-		this.add(ploS);
+		tabbedPane.addTab("Hand Selector",ploS);
+		tabbedPane.addTab("Flop Selector",fs);
+		
+		this.add(tabbedPane);
+
 		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
