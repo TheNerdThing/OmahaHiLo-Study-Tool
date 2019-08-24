f395612177640b7a8cc3412ee7a41a3395a33bfa
TheNerdThing <GregoryEvevsky@gmail.com>
2019-08-23 20:18:37 -0400

    created layout of CalcResults Class

--- PLO8 FlopZilla/src/CalcResults.java
+++ PLO8 FlopZilla/src/CalcResults.java
@@ -1,11 +1,39 @@
 
 /**
  * @author Gregory Evevsky
- * TODO
+ * TODO Impliment class
  */
-public final class CalcResults {
+public final  class CalcResults {
+
+	private static final int TOTAL_FLOPS = 19600; // total different number of 3 card combos that can be made with a deck of cards
 
 	
+	public static void calcResults(Card[] hand, Card[] board, ResultsPane update) {
+
+		if(board.length == 3) {
+			update.updateTable(flopResults( hand, board), null);
+		}else if(board.length == 4) {
+			update.updateTable(turnResults(hand, board), null);
+		}else if(board.length == 5) {
+			update.updateTable(riverResults(hand, board), null);
+		}else if(board.length == 0) {
+			update.updateTable(preFlopResults(hand, board), null);
+		}else {
+			System.out.println("not enough info to calc results");
+		}
+	
+	}
+
+	
+	private static Object[][] riverResults  (Card[] hand, Card[] board){return null;}
+	private static Object[][] preFlopResults(Card[] hand, Card[] board){return null;}
+	private static Object[][] turnResults   (Card[] hand, Card[] board){return null;}
+	private static Object[][] flopResults   (Card[] hand, Card[] board){return null;}
+	private static boolean hasNutFLushDraw (Card[] hand, Card[] board) {return false;}
+	private static boolean hasNutLowDraw   (Card[] hand, Card[] board) {return false;}
+	private static boolean hasNonNutLowDraw(Card[] hand, Card[] board) {return false;}
+	private static boolean hasStr8Draw     (Card[] hand, Card[] board) {return false;}
+	private static boolean hasPair         (Card[] hand, Card[] board) {return false;}
 	
 	
 }
---
+++ PLO8 FlopZilla/src/Card.java
@@ -0,0 +1,33 @@
+
+/**
+ * @author Gregory Evevsky
+ * helper class to help make sence of the stats
+ */
+public class Card {
+
+	private int rank; 
+	private char suite;
+	/**
+	 * @param rank should be between 1 and 14
+	 * @param suite
+	 */
+	public Card(int rank, char suite) {
+		super();
+		this.rank = rank;
+		this.suite = suite;
+	}
+	/**
+	 * @return the rank
+	 */
+	public int getRank() {
+		return rank;
+	}
+	/**
+	 * @return the suite
+	 */
+	public char getSuite() {
+		return suite;
+	}  
+	
+	
+}
